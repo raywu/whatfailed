2f6810ee6853c88cf997be992167abe9300a63d4
Eric Rafaloff <erafaloff@gmail.com>
2011-07-09 18:21:39 -0400

    CSS and view changes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,18 @@
 module PostsHelper
   
+# todo: Check for http, if not include it  
+
   def title(post)
     if post.url.blank?
-		 link_to post.title, post
-		  else
-		 link_to post.title, post.url
+      link_to post.title, post
+    else
+      link_to post.title, post.url
 		end
+	end
+	
+	def link(post)
+	    if post.url?
+	      "(#{URI.parse(post.url).host})"
+	    end
 	end
 end
