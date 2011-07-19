4c12e97dd2b68f672c862eed2a7891839e2ebd77
Eric Rafaloff <erafaloff@gmail.com>
2011-07-19 14:10:10 -0400

    Added date formatting ie; '2 days ago'

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -15,4 +15,20 @@ module PostsHelper
 	      "(#{URI.parse(post.url).host})"
 	    end
 	end
+	
+	def relative_time(posttime)
+    diff_seconds = Time.now - posttime
+    case diff_seconds
+      when 0 .. 59
+        "#{(diff_seconds).to_i} seconds ago"
+      when 60 .. (3600-1)
+        "#{(diff_seconds/60).to_i} minutes ago"
+      when 3600 .. (3600*24-1)
+        "#{(diff_seconds/360).to_i} hours ago"
+      when (3600*24) .. (3600*24*30) 
+        "#{(diff_seconds/(3600*24)).to_i} days ago"
+      else
+        posttime.strftime("%m/%d/%Y")
+    end
+  end
 end
