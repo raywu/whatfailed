24fe48c0f386217fed1e09f2e88ea605f820ed15
Eric Rafaloff <erafaloff@gmail.com>
2011-07-19 15:02:58 -0400

    Added auth check for tags, removed tag resources

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  before_filter :authed, :except => [:index, :show]
   
   def index
     @tags = Tag.all
@@ -22,57 +23,6 @@ class TagsController < ApplicationController
       format.html # show.html.erb
       format.xml  { render :xml => @tag }
       format.json { render :json => @tags.map(&:attributes) }
-    end
-  end
-
-  def new
-    @tag = Tag.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @tag }
-    end
-  end
-
-  def edit
-    @tag = Tag.find(params[:id])
-  end
-
-  def create
-    @tag = Tag.new(params[:tag])
-
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to(@tag, :notice => 'Tag was successfully created.') }
-        format.xml  { render :xml => @tag, :status => :created, :location => @tag }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @tag.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    @tag = Tag.find(params[:id])
-
-    respond_to do |format|
-      if @tag.update_attributes(params[:tag])
-        format.html { redirect_to(@tag, :notice => 'Tag was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @tag.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @tag = Tag.find(params[:id])
-    @tag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(tags_url) }
-      format.xml  { head :ok }
     end
   end
 end
