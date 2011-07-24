0a904dff680339a5a4c7dc07b68de06631a53dc9
Ray Wu <raymond.cj.wu@gmail.com>
2011-07-23 20:08:57 -0400

    modified SessionsController and user.rb to incorporate user email, then redirects to registrations. registrations still doesn't work for Twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"])
+    user = User.create_with_omniauth(auth)
+      if user.email.blank?
+        redirect_to "registrations"
+        # this is where we need to get Email from users for Twitter
+      else
+        session[:user_id] = user.id
+        redirect_to root_url, :notice => "Signed in!"
+    end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110716212459) do
+ActiveRecord::Schema.define(:version => 20110723230650) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20110716212459) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "cached_slug"
+    t.string   "email"
   end
 
 end
