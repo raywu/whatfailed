2b2e8ce07138c6d51617c2261f2ecfefc2eaa23b
Ray Wu <raymond.cj.wu@gmail.com>
2011-07-26 01:16:03 -0400

    still no avail. need to figure out why it is skipping registrations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"]) || User.create_with_omniauth(auth)   
-      if user.email.blank?
+      if user.email = nil
         session[:auth] = auth.except('extra')
         render :action => 'registrations' and return if user.email
         #this is a good place to render registration in a pop-up window or somehow javascript drop-down to let people enter email address
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     @user = User.find(params[:id])
     
     respond_to do |format|
-      if @user.update_attributes(params[:email])
+      if @user.update_email(params[:id])
         format.html { render :action => 'create' }
       end
     end
