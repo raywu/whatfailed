16c76e02c4dd8288c4afb030c6dd18b843c4c4f6
Ray Wu <raymond.cj.wu@gmail.com>
2011-07-26 00:31:31 -0400

    sessions controller now works with registration action, but still by passing email update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,24 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"]) || User.create_with_omniauth(auth)
       if user.email.blank?
         session[:auth] = auth.except('extra')
-        render :action => 'registrations'and return if user.email.blank? false
+        render :action => 'registrations' and return if user.email
         #this is a good place to render registration in a pop-up window or somehow javascript drop-down to let people enter email address
+      end
+      session[:user_id] = user.id      
+      redirect_to root_url, :notice => "Signed in!"
     end
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+
+  def registrations
+    @user = User.find(params[:id])
+    
+    respond_to do |format|
+      if @user.update_attributes(params[:id])
+        format.html { render :action => 'create' }
+      end
+    end
   end
-
-
+    
+  
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
