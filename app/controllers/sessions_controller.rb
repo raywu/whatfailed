cdfe1d3241b67d65098de9633626421472be09c8
Ray Wu <raymond.cj.wu@gmail.com>
2011-07-25 23:47:36 -0400

    modified Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"])
-    user = User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"]) || User.create_with_omniauth(auth)
       if user.email.blank?
-        redirect_to "registrations"
-        # this is where we need to get Email from users for Twitter
-      else
-        session[:user_id] = user.id
-        redirect_to root_url, :notice => "Signed in!"
+        session[:auth] = auth.except('extra')
+        render :action => 'registrations'and return if user.email.blank? false
+        #this is a good place to render registration in a pop-up window or somehow javascript drop-down to let people enter email address
     end
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
   end
+
 
   def destroy
     session[:user_id] = nil
