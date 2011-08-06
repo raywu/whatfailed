d7b10596eec1ee63c27cf8b7833cf11260925f80
Ray Wu <raymond.cj.wu@gmail.com>
2011-08-06 18:30:43 -0400

    almost done with appending email to Twitter account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"]) || User.create_with_omniauth(auth)   
-      if user.save! == false
+    user = User.find_by_provider_and_uid_and_email(auth["provider"], auth["uid"], auth["user_info"]["email"]) || user = User.create_with_omniauth(auth)   
+    if user.email.nil?
+        session[:user_id] = user.id
         session[:auth] = auth.except('extra')
-        render :action => 'register'        #this is a good place to render registration in a pop-up window or somehow javascript drop-down to let people enter email address
-      end   
+        redirect_to :registration #and return if user.email
+        #this is a good place to render registration in a pop-up window or somehow javascript drop-down to let people enter email address
+    elsif   
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Signed in!"
     end
+  end
 
   def register
-    @user = User.find(session[:auth])
-    
+    @user = User.find(session[:user_id])
+              logger.debug session[:use_id].inspect
+
     respond_to do |format|
-      if @user.update_email(params[:session][:email])
-        format.html { render :action => 'create' }
+      if @user.update_attributes(params[:email])
+        format.html { render 'sessions/register' }
       end
     end
   end
