fe232606089edc043d45380909680f4b604ece3b
Ray Wu <raymond.cj.wu@gmail.com>
2011-09-26 18:39:26 -0400

    added new FB key to Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'ml4npE0q6pEttXMj4BYmw', 'E3B4LsFU3aG6yJLVCSQPmRUKwnxiRUSQZGHa4yKok'
-  provider :facebook, '132331646850398', '6816c59bc768d8e43ea08df55c49f28d'
+  provider :facebook, '240443122674939', 'df4fd18da2c46e5479d26a26ad76900b'
 end
