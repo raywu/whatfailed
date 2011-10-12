f0c67b0fa469d5ea5810282dc2fa7bec8d9cf3a0
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-12 05:24:49 -0400

    changed fb api settings back to box on master

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '240443122674939', 'df4fd18da2c46e5479d26a26ad76900b'
+  provider :facebook, '132331646850398', '6816c59bc768d8e43ea08df55c49f28d'
   provider :twitter, 'XwxvmdOPvSza0gklqpliw', 'yL1adDZSS7vk3c01gmH4OpWMmSc39oySilAYS7d2RG0'
 end
