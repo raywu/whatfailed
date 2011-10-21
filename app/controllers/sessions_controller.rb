6e18e3e74b6e95808d4bacebe81761764e5365e4
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-20 23:53:20 -0400

    added rescue redirect to avoid no referrer session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to :back, :notice => "Signed in!"
+  rescue ActionController::RedirectBackError
+    redirect_to :root
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to :back, :notice => "Signed out!"
+  rescue ActionController::RedirectBackError
+    redirect_to :root
   end
 end
