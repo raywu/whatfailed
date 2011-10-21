e9e2d2bda4b3901df06668b5e8f973afe1584fc7
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-20 23:57:15 -0400

    added rescue to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,15 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to :back, :notice => "Signed in!" #this is breaking the authentication referrer
+    redirect_to :back, :notice => "Signed in!"
+  rescue ActionController::RedirectBackError
+    redirect_to :root
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to :back, :notice => "Signed out!"
+  rescue ActionController::RedirectBackError
+    redirect_to :root
   end
 end
