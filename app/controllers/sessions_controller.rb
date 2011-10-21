e72fbe16347951243721de1d696fdbf7260727ba
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-20 21:42:25 -0400

    added url display on post index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to :back, :notice => "Signed in!"
+    redirect_to :back, :notice => "Signed in!" #this is breaking the authentication referrer
   end
 
   def destroy
