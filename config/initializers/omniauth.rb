a52863128dda772c3c17862c579b80714d952938
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-22 16:28:05 -0700

    managed conflicts

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-<<<<<<< .merge_file_UeM2TO
   provider :facebook, '132331646850398', '6816c59bc768d8e43ea08df55c49f28d'
-=======
-  provider :facebook, '240443122674939', 'df4fd18da2c46e5479d26a26ad76900b'
->>>>>>> .merge_file_7fsyxL
   provider :twitter, 'XwxvmdOPvSza0gklqpliw', 'yL1adDZSS7vk3c01gmH4OpWMmSc39oySilAYS7d2RG0'
 end
