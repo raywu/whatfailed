dae9ce0f58c786043d57d11622c5bc26aabe4abc
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-23 13:42:03 -0700

    changed fb api key to new app 297086536985505

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '240443122674939', 'e4e37fa8a612b468ef2060a825bcd8ea'
+  provider :facebook, '297086536985505', '23635e929086f8860f2ed82678ad790d'
   provider :twitter, 'XwxvmdOPvSza0gklqpliw', 'yL1adDZSS7vk3c01gmH4OpWMmSc39oySilAYS7d2RG0'
 end
