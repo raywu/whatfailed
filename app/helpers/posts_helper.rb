05592d356dbe734afa9a500304202ce26f101a2c
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-22 17:32:39 -0700

    changed links to open in new tab and configured post index links all to post show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,16 +3,16 @@ module PostsHelper
 # todo: Check for http, if not include it  
 
   def header(post)
-    if post.url.blank?
+    # if post.url.blank?
       link_to post.title, post
-    else
-      link_to post.title, post.url, html_options = { :target => "_blank" }
-		end
+    # else
+    #   link_to post.title, post.url, html_options = { :target => "_blank" }
+		# end
 	end
 	
 	def link(post)
 	    if post.url?
-	      "(#{URI.parse(post.url).host})"
+	      link_to "(#{URI.parse(post.url).host})", post.url, html_options = { :target => "_blank" }
 	    end
 	end
 end
