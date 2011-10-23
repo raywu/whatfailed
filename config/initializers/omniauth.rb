329aa7050f04ffa2225524dc32e40147bd6bd0d7
Ray Wu <raymond.cj.wu@gmail.com>
2011-10-23 13:00:30 -0700

    reconfigured all api keys for launch on whatfailed.us

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '240443122674939', 'df4fd18da2c46e5479d26a26ad76900b'
+  provider :facebook, '240443122674939', 'e4e37fa8a612b468ef2060a825bcd8ea'
   provider :twitter, 'XwxvmdOPvSza0gklqpliw', 'yL1adDZSS7vk3c01gmH4OpWMmSc39oySilAYS7d2RG0'
 end
